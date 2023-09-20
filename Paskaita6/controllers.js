5145e39fd2a7df922dffbe8614bb6acd82ec42c9
Marius <marius.matulevicius1@gmail.com>
2023-09-20 20:09:51 +0300

    paskait 6

---
+++ Paskaita6/controllers.js
@@ -0,0 +1,16 @@
+import Person from "./models/Person.js";
+
+export async function addNewPerson(req, res) {
+  const { name, age } = req.body;
+  try {
+    const newPerson = new Person({
+      name,
+      age,
+    });
+    await newPerson.save();
+
+    res.json(newPerson);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
